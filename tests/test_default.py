86fed75127ed1de02f78a249c9f13a0076d4c067
Marcin Praczko <marcin.praczko@gmail.com>
2017-03-20 13:06:00 +0000

    Add initial testing/validating script

--- tests/test_default.py
+++ tests/test_default.py
@@ -4,8 +4,14 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     '.molecule/ansible_inventory').get_hosts('all')
 
 
-def test_hosts_file(File):
-    f = File('/etc/hosts')
+def test_named_running_and_enabled(Service):
+    named = Service("named")
+    assert named.is_running
+    assert named.is_enabled
+
+
+def test_included_conf_file(File):
+    f = File('/etc/named/included.conf')
 
     assert f.exists
     assert f.user == 'root'
